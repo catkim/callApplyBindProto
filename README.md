7455d0e29046019df8ca8b79816f3f3c597371a0
Catherine Kim <abbytherabbit@gmail.com>
2019-11-18 21:12:42 -0800

    updating format

--- README.md
+++ README.md
@@ -4,3 +4,12 @@ I made this repo to go over Call, Apply, Bind and Prototypical Inheritance in si
 let buttonsArray = [].slice.call(document.querySelectAll('button'))
 
 To me it was a bit confusing seeing call used in both ways. I wanted to feel more confident using these methods so I did some reading, watched some great videos, and played around with code until I felt I had a more solid understanding.  Hopefully you will too after looking over my code and I'll leave the links to the videos.
+
+Read the comments and then remove this text one line at a time, save and check console log:
+// ---->  remove double backslash and instruction <---
+
+links to videos:
+
+https://www.youtube.com/watch?v=c0mLRpw-9rI  -- call apply bind - Techsith (this guy is great!)
+https://www.youtube.com/watch?v=YkoelSTUy7A&t=1s -- Prototype basics - FunFunFunction - (he's great too)
+https://www.youtube.com/watch?v=g2WcckBB_q0 -- Bind Explained in Javascript
--- script.js
+++ script.js
@@ -26,65 +26,65 @@
 console.dir(pieMaker);
 
 //we can invoke the pieMaker method fillingRequest and pass in an apple filling:
-pieMaker.fillingRequest("apple");
+// ---->  remove double backslash and instruction text<---  pieMaker.fillingRequest("apple");
 
 //if we console.log we see our apple pie being made and the pieMarker object has a new property "filling" with a value of apple:
-console.log(pieMaker);
+// ---->  remove double backslash and instruction <---  console.log(pieMaker);
 
 // all of this is pretty straight forward we can call pieMaker again using the crustRequest method and pass a value for crust:
-pieMaker.crustRequest('lattice');
+ // ---->  remove double backslash and instruction text <--- pieMaker.crustRequest('lattice');
 
 // now we can look at pieMaker and see that it has the crust property with the lattice value:
-console.dir(pieMaker);
+// ---->  remove double backslash and instruction text <--- console.dir(pieMaker);
 
 // super straightforward and simple right?
 
 //so what if we wanted to make another object literal for a pie of a different flavor and we want to reuse the code already written in pieMaker. We can use "call" to bind the context of "this" to the new object and pass the parameters for filling and crust:
 
-let specialPie = {}; //create the object
+ // ---->  remove double backslash and instruction <--- let specialPie = {}; //create the object
 
-pieMaker.fillingRequest.call(specialPie, "blueBerry"); //invoke the fillingRequest method in pieMaker on the specialPie object ("this") and pass in "blueBerry"
+// ---->  remove double backslash and instruction <--- pieMaker.fillingRequest.call(specialPie, "blueBerry"); //invoke the fillingRequest method in pieMaker on the specialPie object ("this") and pass in "blueBerry"
 
-pieMaker.crustRequest.call(specialPie, "criss-cross");//invoke the crustRequest method in pieMaker on the specialPie object ("this") and pass in "criss-cross"
-console.log(specialPie);
+// ---->  remove double backslash and instruction <--- pieMaker.crustRequest.call(specialPie, "criss-cross");//invoke the crustRequest method in pieMaker on the specialPie object ("this") and pass in "criss-cross"
+// ---->  remove double backslash and instruction <--- console.log(specialPie);
 
 //to pass in arguments in an array we can use apply
-pieMaker.addMagicWords.apply(specialPie, ["please", "thanks"]);// invoke the addMagicWords method on the specialPie object and pass in an array of strings
-console.dir(specialPie);
+// ---->  remove double backslash and instruction <--- pieMaker.addMagicWords.apply(specialPie, ["please", "thanks"]);// invoke the addMagicWords method on the specialPie object and pass in an array of strings
+// ---->  remove double backslash and instruction <--- console.dir(specialPie);
 
 // so hopefully call and apply are a little less murky but of course there's more. :)
 
 //if you look at the last object in the console log (specialPie) you will see it has three properies with values and another object called __proto__ and it has a bunch of built in methods.
 
 //let's create another pie object
- let extraSpecialPie = {}
+// ---->  remove double backslash and instruction <---  let extraSpecialPie = {}
 
  //instead of using call or apply let's use the handy setPrototypeOf method
- Object.setPrototypeOf(extraSpecialPie, pieMaker); // it's called with the global Object and setPrototypeOf method and you first pass in the object you want to apply the prototype on and then the object you are borrowing from.
+ // ---->  remove double backslash and instruction <--- Object.setPrototypeOf(extraSpecialPie, pieMaker); // it's called with the global Object and setPrototypeOf method and you first pass in the object you want to apply the prototype on and then the object you are borrowing from.
 
 //inspect the extraSpecialPie object and you'll see it has all the properies and methods from the object we borrowed from on the __proto__ object as well as the built in __proto__ object.
- console.dir(extraSpecialPie);
+ // ---->  remove double backslash and instruction <--- console.dir(extraSpecialPie);
 
 //when using call, apply or bind you only get a specific method set on the object with setPrototypeOf you get the whole shabang
 
 //Looking at the taste property on the extraSpecialPie object
-console.log(extraSpecialPie.taste);
+// ---->  remove double backslash and instruction <--- console.log(extraSpecialPie.taste);
 
 //if we update the pieMaker object with a request for an anchovy filling
-pieMaker.fillingRequest('anchovy');
-pieMaker.taste = "salty and terrible"
+// ---->  remove double backslash and instruction <--- pieMaker.fillingRequest('anchovy');
+// ---->  remove double backslash and instruction <--- pieMaker.taste = "salty and terrible"
 
 //our extraSpecialPie now also has a taste of salty and terrible
-console.log('extraSpecialPie.taste is', extraSpecialPie.taste);
+// ---->  remove double backslash and instruction <--- console.log('extraSpecialPie.taste is', extraSpecialPie.taste);
 
 //but if we add some fairy dust to the extraSpecialPie
-extraSpecialPie.fairyDust = true;
+// ---->  remove double backslash and instruction <--- extraSpecialPie.fairyDust = true;
 
 //we see it on our object
-console.dir(extraSpecialPie);
+// ---->  remove double backslash and instruction <--- console.dir(extraSpecialPie);
 
 //but not on the pieMarker object
-console.log('pieMaker has ', pieMaker.fairyDust, 'for the fairyDust property');
+// ---->  remove double backslash and instruction <--- console.log('pieMaker has ', pieMaker.fairyDust, 'for the fairyDust property');
 
 //this is called delegation
 
@@ -93,13 +93,7 @@ function eatPie(){
   console.log(`Yum, enjoying this ${this.filling} pie`);
 }
 
-let yum = eatPie.bind(specialPie);
-yum();
+// ---->  remove double backslash and instruction <--- let yum = eatPie.bind(specialPie);
+// ---->  remove double backslash and instruction <--- yum();
 
 //hope my explaination helped you understand call, apply bind and proto
-
-//links to videos:
-
-// https://www.youtube.com/watch?v=c0mLRpw-9rI  -- call apply bind - Techsith (this guy is great!)
-// https://www.youtube.com/watch?v=YkoelSTUy7A&t=1s -- Prototype basics - FunFunFunction - (he's great too)
-// https://www.youtube.com/watch?v=g2WcckBB_q0 -- Bind Explained in Javascript
